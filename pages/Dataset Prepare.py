5e03ade948aef766f66a309dc68819b7656452d3
Parazice <161585132+Parazice@users.noreply.github.com>
2025-03-16 23:30:39 +0700

    Finished Dataset and Model Training pages

--- pages/Dataset Prepare.py
+++ pages/Dataset Prepare.py
@@ -1,2 +1,228 @@
 import streamlit as st
-st.write("xd")
+import pandas as pd
+
+st.title("Dataset Preparation")
+mlTab, nnTab = st.tabs(["Machine Learning", "Neural Networks"])
+with mlTab:
+    st.header("Find the dataset")
+    st.write("หา Dataset ที่สนใจจะนำมาทำ Machine Learning ในการคาดการณ์ข้อมูล โดยผมหามาจาก [kaggle.com](https://www.kaggle.com/)\n")
+    st.write("[Click to download my dataset](https://www.kaggle.com/datasets/stealthtechnologies/predict-people-personality-types)")
+
+    df = pd.DataFrame(pd.read_csv("./training/data.csv"))
+    st.subheader("Predict People Personality Types Dataset")
+    st.dataframe(df)
+
+    st.subheader("Comlumn Description")
+    columns = df.columns
+    description = pd.DataFrame(
+        {
+            "Column Name": list(columns),
+            "Description": [
+                "A continuous variable representing the age of the individual.",
+                "A categorical variable indicating the gender of the individual. Possible values are 'Male' and 'Female'.",
+                "A binary variable, A value of 1 indicates the individual has at least a graduate-level education (or higher), and 0 indicates an undergraduate, high school level or Uneducated.",
+                "A categorical variable representing the individual's primary area of interest.",
+                "A continuous variable ranging from 0 to 10, representing the individual's tendency toward introversion versus extraversion. Higher scores indicate a greater tendency toward extraversion.",
+                "A continuous variable ranging from 0 to 10, representing the individual's preference for sensing versus intuition. Higher scores indicate a preference for sensing.",
+                "A continuous variable ranging from 0 to 10, indicating the individual's preference for thinking versus feeling. Higher scores indicate a preference for thinking.",
+                "A continuous variable ranging from 0 to 10, representing the individual's preference for judging versus perceiving. Higher scores indicate a preference for judging.",
+                "Target that contains People Personality Type"
+            ],
+            "Example": [
+                df[i].unique() if df[i].dtypes == "object" else df[i].iloc[0]
+                for i in df.columns
+            ],
+        }
+    )
+    st.dataframe(description, hide_index=True)
+
+    st.header("Making Dataset Incomplete")
+    st.write("เนื่องจาก Dataset ที่ดาวน์โหลดมานั้นเป็น Dataset ที่สมบูรณ์ ผมจึงใช้โค้ดนี้ในการทำให้ Dataset เกิดความเสียหายด้วยการทำให้ข้อมูลบางส่วนเป็น Null")
+    st.code("""
+    import pandas as pd
+    import numpy as np
+
+    # Load dataset
+    file_path = "data.csv"
+    df = pd.read_csv(file_path)
+
+    # Set a percentage of missing values (e.g., 10% of the data)
+    missing_percentage = 0.15
+    num_missing = int(missing_percentage * df.size)
+
+    # Randomly select positions to set as NaN
+    np.random.seed(42)  # For reproducibility
+    missing_indices = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
+    missing_indices = np.random.choice(len(missing_indices), num_missing, replace=False)
+
+    for idx in missing_indices:
+        row, col = divmod(idx, df.shape[1])
+        df.iat[row, col] = np.nan
+
+    # Save the modified dataset
+    modified_file_path = "data_missing.csv"
+    df.to_csv(modified_file_path, index=False)
+
+    # Return the path of the modified file
+    modified_file_path
+
+    """)
+    st.subheader("Dataset After Add Null Values")
+    df1 = pd.DataFrame(pd.read_csv("./training/data_missing.csv"))
+    st.dataframe(df1)
+
+    st.header("Data Cleaning")
+    st.subheader("Drop Null Values")
+    st.code("""
+    import pandas as pd
+    import pickle
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.svm import SVC
+    from sklearn.metrics import accuracy_score
+            
+    kd = pd.read_csv('data_missing.csv')
+    #checking Null values
+    kd.isnull().any()
+
+    #drop Null values
+    kd = kd.dropna()
+   
+    """)
+    st.subheader("Data Mapping")
+    st.code("""
+    def map_data(column, value):
+        #dictionary for data mapping
+        mapping_dict = {
+            "Gender": {"Male": 0, "Female": 1},
+            "Interest": {'Arts': 0, 'Technology': 1, 'Sports': 2, 'Others': 3, 'Unknown': 4},
+            "Personality": {'ISFP': 0, 'ISFJ': 1, 'INFP': 2, 'ESFJ': 3, 'INTJ': 4, 'INFJ': 5, 'ESTP': 6, 'ISTJ': 7,
+        'ISTP': 8, 'ENTP': 9, 'ESFP': 10, 'INTP': 11, 'ENTJ': 12, 'ESTJ': 13, 'ENFJ': 14, 'ENFP': 15}
+        }
+        return mapping_dict[column].get(value)
+    for column in kd.columns:
+        #if the value is string
+        if kd[column].dtype == 'object':
+            #replace with mapping dictionary's index
+            kd[column] = kd[column].apply(lambda x: map_data(column, x))
+    kd = kd.astype(int)
+    kd
+    """)
+    st.write("เท่านี้ Dataset ก็พร้อมนำไปใช้ในการทำ Machine Learning แล้ว")
+
+with nnTab:
+    st.header("Find the dataset")
+    st.write("หา Dataset ที่สนใจจะนำมาทำ Machine Learning ในการคาดการณ์ข้อมูล โดยผมหามาจาก [kaggle.com](https://www.kaggle.com/)\n")
+    st.write("[Click to download my dataset](https://www.kaggle.com/datasets/rabieelkharoua/predict-online-gaming-behavior-dataset/data)")
+
+    df = pd.DataFrame(pd.read_csv("./training/online_gaming_behavior_dataset.csv"))
+    st.subheader("🎮 Predict Online Gaming Behavior Dataset")
+    st.dataframe(df)
+
+    st.subheader("Comlumn Description")
+    columns = df.columns
+    description = pd.DataFrame(
+        {
+            "Column Name": list(columns),
+            "Description": [
+                "Unique identifier for each player.",
+                "Age of the player.",
+                "Gender of the player.",
+                "Geographic location of the player.",
+                "Genre of the game the player is engaged in.",
+                "Average hours spent playing per session.",
+                "Indicates whether the player makes in-game purchases (0 = No, 1 = Yes).",
+                "Difficulty level of the game.",
+                "Number of gaming sessions per week.",
+                "Average duration of each gaming session in minutes.",
+                "Current level of the player in the game.",
+                "Number of achievements unlocked by the player.",
+                "Categorized engagement level reflecting player retention ('High', 'Medium', 'Low')."
+            ],
+            "Example": [
+                df[i].unique() if df[i].dtypes == "object" else df[i].iloc[0]
+                for i in df.columns
+            ],
+        }
+    )
+    st.dataframe(description, hide_index=True)
+
+    st.header("Making Dataset Incomplete")
+    st.write("เนื่องจาก Dataset ที่ดาวน์โหลดมานั้นเป็น Dataset ที่สมบูรณ์ ผมจึงใช้โค้ดนี้ในการทำให้ Dataset เกิดความเสียหายด้วยการทำให้ข้อมูลบางส่วนเป็น Null")
+    st.code("""
+    import pandas as pd
+    import numpy as np
+
+    # Load dataset
+    file_path = "./training/online_gaming_behavior_dataset.csv"
+    df = pd.read_csv(file_path)
+
+    # Set a percentage of missing values (e.g., 10% of the data)
+    missing_percentage = 0.15
+    num_missing = int(missing_percentage * df.size)
+
+    # Randomly select positions to set as NaN
+    np.random.seed(42)  # For reproducibility
+    missing_indices = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
+    missing_indices = np.random.choice(len(missing_indices), num_missing, replace=False)
+
+    for idx in missing_indices:
+        row, col = divmod(idx, df.shape[1])
+        df.iat[row, col] = np.nan
+
+    # Save the modified dataset
+    modified_file_path = "online_gaming_missing.csv"
+    df.to_csv(modified_file_path, index=False)
+
+    # Return the path of the modified file
+    modified_file_path
+
+    """)
+    st.subheader("Dataset After Add Null Values")
+    df1 = pd.DataFrame(pd.read_csv("./training/online_gaming_missing.csv"))
+    st.dataframe(df1)
+
+    st.header("Data Cleaning")
+    st.subheader("Drop Null Values")
+    st.code("""
+    import pandas as pd
+    import pickle
+    import numpy as np
+    import keras
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+    from keras import layers
+    from keras.optimizers import Adam
+    from keras.utils import to_categorical
+            
+    kd = pd.read_csv('online_gaming_missing.csv')
+    #checking Null values
+    kd.isnull().any()
+
+    #drop Null values
+    kd = kd.dropna()
+   
+    """)
+    st.subheader("Data Mapping")
+    st.code("""
+    def map_data(column, value):
+        mapping_dict = {
+            "Gender": {"Male": 0, "Female": 1},
+            "Location": {"USA": 0, "Europe": 1, "Asia": 2, "Other": 3},
+            "GameGenre": {'Strategy': 0, 'Simulation': 1, 'Action': 2, 'RPG': 3, 'Sports': 4},
+            "GameDifficulty": {'Easy': 0, 'Medium': 1, 'Hard': 2},
+            "EngagementLevel": {'Low': 0, 'Medium': 1, 'High': 2}
+        }
+        return mapping_dict[column].get(value)
+    for column in kd.columns:
+        #if the value is string
+        if kd[column].dtype == 'object':
+            #replace with mapping dictionary's index
+            kd[column] = kd[column].apply(lambda x: map_data(column, x))
+    kd = kd.astype(int)
+    kd
+    """)
+    st.write("เท่านี้ Dataset ก็พร้อมนำไปใช้ในการทำ Machine Learning แล้ว")
+    
--- pages/Model Training.py
+++ pages/Model Training.py
@@ -1,2 +1,131 @@
 import streamlit as st
-st.write("dx")
+
+st.title("Model Training")
+mlTab, nnTab = st.tabs(["Machine Learning", "Neural Networks"])
+with mlTab:
+    st.header("Random Forest Classifier Training")
+    st.subheader("Data Scailing and Dividing")
+    st.code("""
+    #devided data for training column and answer column
+    rfcX = kd.drop(columns=['Personality'])
+    rfcy = kd['Personality']
+
+    #scaling data
+    scaler = MinMaxScaler()
+    rfcX_scaled = pd.DataFrame(scaler.fit_transform(rfcX), columns=rfcX.columns)
+    rfcX_scaled
+    """)
+    st.subheader("Training, Evalutaing, and Saving Model")
+    st.code("""
+    #split data for training and testing
+    rfcX_train, rfcX_test, rfcy_train, rfcy_test = train_test_split(rfcX_scaled, rfcy, test_size=0.2, random_state=42)
+
+    #model training
+    rfc_model = RandomForestClassifier(n_estimators=150, random_state=42, class_weight="balanced")
+    rfc_model.fit(rfcX_train, rfcy_train)
+
+    #model tesing
+    rfcy_pred = rfc_model.predict(rfcX_test)
+
+    #model evaluating
+    rfc_acc = accuracy_score(rfcy_test, rfcy_pred)
+
+    print(f"Random Forest Classifier Accuracy: {rfc_acc:.2f}\\n")
+
+    #save
+    pickle.dump(rfc_model, open('rfc_model.pkl', 'wb'))
+    pickle.dump(scaler, open('rfc_scaler.pkl', 'wb'))
+    """)
+
+    st.header("SVM Training")
+    st.subheader("Data Scailing and Dividing")
+    st.code("""
+    #devided data for training column and answer column
+    svmX = kd.drop(columns=['Personality'])
+    svmy = kd['Personality']
+
+    #scaling data
+    scaler = MinMaxScaler()
+    svmX_scaled = pd.DataFrame(scaler.fit_transform(svmX), columns=svmX.columns)
+    svmX_scaled
+    """)
+    st.subheader("Training, Evalutaing, and Saving Model")
+    st.code("""
+    #split data for training and testing
+    svmX_train, svmX_test, svmy_train, svmy_test = train_test_split(svmX_scaled, svmy, test_size=0.2, random_state=42)
+
+    #model training
+    svm_model = SVC(kernel="poly", random_state=42, class_weight="balanced")
+    svm_model.fit(svmX_train, svmy_train)
+            
+    #model tesing
+    svmy_pred = svm_model.predict(svmX_test)
+
+    #model evaluating
+    svm_acc = accuracy_score(svmy_test, svmy_pred)
+
+    print(f"SVM Accuracy: {svm_acc:.2f}\\n")
+
+    #save
+    pickle.dump(rfc_model, open('svm_model.pkl', 'wb'))
+    pickle.dump(scaler, open('svm_scaler.pkl', 'wb'))
+    """)
+
+with nnTab:
+    st.header("FNN Training")
+    st.subheader("Data Scailing and Dividing")
+    st.code("""
+    #devided data for training column and answer column
+    X = kd.drop(columns=['EngagementLevel', 'PlayTimeHours', 'PlayerID'])
+    y = kd['EngagementLevel']
+
+    #scaling data
+    scaler = MinMaxScaler()
+    X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+    X_scaled
+    """)
+    st.subheader("Training, Evalutaing, and Saving Model")
+    st.code("""
+    #split data for training and testing
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+    y_train = to_categorical(y_train, num_classes=num_classes)
+    y_test = to_categorical(y_test, num_classes=num_classes)
+
+    #model training
+    model = keras.Sequential([
+        layers.Input(shape=(X_train.shape[1],)),
+        layers.Dense(128, activation="relu"),
+        layers.Dropout(0.3),
+        layers.Dense(64, activation="relu"),
+        layers.Dropout(0.2),
+        layers.Dense(32, activation="relu"),
+        layers.Dense(3, activation="softmax")
+    ])
+    # Use a lower learning rate for better convergence
+    optimizer = Adam(learning_rate=0.001)
+
+    model.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    #model tesing
+    history = model.fit(
+        X_train, y_train,
+        epochs=20, 
+        batch_size = 16,
+        validation_split = 0.2
+
+    )
+
+    #model evaluating
+    test_loss, test_accuracy = model.evaluate(X_test, y_test)
+    from math import ceil
+    print(f"Batch : {ceil(((len(X_train) - int((len(X_train) * 0.2)))) / 32)}")
+    print(f"Test accuracy: {test_accuracy:.2f}")
+
+    #save
+    model.save('fnn_model.keras')
+    pickle.dump(scaler, open('fnn_scaler.pkl', 'wb'))
+    """)

0cff4183c0bf7945aac31acd256cb17c0cc4adf3
Parazice <161585132+Parazice@users.noreply.github.com>
2025-03-16 22:00:05 +0700

    add model training docs page

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
+pages = {
+    "Developing": [
+        st.Page("pages/app.py", title="Dataset Preparing"),
+        st.Page("pages/Model Training.py", title="Model Training"),
+    ],
+    "Demo": [
+        st.Page("pages/Machine Learning Predict.py", title="Machine Learning"),
+        st.Page("pages/Neural Networks Predict.py", title="Neural Network"),
+    ],
+    # "Reference": [
+    #     st.Page("views/reference_page.py", title="Reference"),
+    # ],
+}
+
+pg = st.navigation(pages, position="sidebar")
+pg.run()
 st.write("xd")
